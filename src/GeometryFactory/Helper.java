5f0643b3ee5abbd9fa869bde9c5ed6986eabf2aa
Amandus <amandusbutzer@gmx.de>
2018-09-19 18:14:07 +0200

    Create Helper Class to outsource function

---
+++ src/GeometryFactory/Helper.java
@@ -0,0 +1,30 @@
+package GeometryFactory;
+
+public class Helper {
+    /**
+     * Checks if wkt conform and returns the wkt type or null if not wkt conform
+     * @param dimension - the dimension of the object
+     * @param mValue - the measure value of the object
+     * @return {String|null} - the wkt type to return
+     */
+    public static String getWktType(int dimension, Double mValue) {
+        String wktType = "";
+        if (dimension < 4 && dimension > 1) {
+            if (dimension == 2 && mValue == null) {
+                wktType = "Normal";
+            } else if (dimension == 3) {
+                wktType = "Z";
+            }
+            if (mValue != null) {
+                wktType += "M";
+            }
+        } else {
+            wktType = null;
+        }
+        return wktType;
+    }
+
+    public static String getWktType(int dimension) {
+        return getWktType(dimension, null);
+    }
+}
