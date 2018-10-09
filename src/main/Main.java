ee2849346108825d0fbf6d0becd8ae39851c9544
Amandus <amandusbutzer@gmx.de>
2018-10-09 13:29:02 +0200

    Structure Main.java & add Polygon and first Feature test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 
 import Exceptions.DimensionalException;
+import Exceptions.InvalidPolygonException;
 import Exceptions.WktInvalidException;
 import Exceptions.WKTRepresentationException;
+import FeaturePackage.PointFeature;
 import GeometryFactory.Point;
 import GeometryFactory.GeometryFactory;
 import GeometryFactory.Line;
@@ -11,7 +13,7 @@ import GeometryFactory.Polygon;
 public class Main {
 
 	public static void main(String[] args) {
-		// Yay, it works like this
+		System.out.println("Point Tests:");
 		Point p = GeometryFactory.createPoint(23.4, 123.2, 3, 42, 1, 54, 3);
 		Point m = GeometryFactory.createPointM( 2, 1232, 33);
 		Point n = null;
@@ -22,6 +24,8 @@ public class Main {
 		}
 
 		System.out.println(p.getWktType());
+		// use assertion tests ?
+		// assert p.getWktType() == null;
 		System.out.println(p.getDimension());
 		System.out.println(p.getX());
 		p.setX(10.1);
@@ -38,22 +42,6 @@ public class Main {
 			e1.printStackTrace();
 		}
 
-		Point x1 = GeometryFactory.createPoint(3,4);
-		Point x2 = GeometryFactory.createPoint(4,5);
-		Point x3 = GeometryFactory.createPoint(6,2);
-
-		Point[] polyPoints = {x1,x2,x3,x1};
-
-		Polygon poly = GeometryFactory.createPolygon(polyPoints);
-
-		for (Point x : poly.getPoints()) {
-			try {
-				System.out.println(x.getWKT());
-			} catch (WKTRepresentationException e) {
-				e.printStackTrace();
-			}
-		}
-
 		System.out.println(n.is(m));
 		n.setX(32);
 		System.out.println(n.getX());
@@ -61,7 +49,7 @@ public class Main {
 
 		Point q = null;
 		try {
-			q = GeometryFactory.createPoint("POINT M(30 10 9 2)");
+			q = GeometryFactory.createPoint("POINT ZM(30 10 9 2)");
 		} catch (WktInvalidException e) {
 			e.printStackTrace();
 		}
@@ -88,15 +76,19 @@ public class Main {
 			System.out.print(cord + ", ");
 		}
 		System.out.println("Point q's value: " + q.getLrsValue());
+
 //		TODO test functions of Point (eg. Point m or p) like m.getX()
 //		System.out.println(m.getLrsValue());
 //		m.getCoordinates();
 //		Line pmLine = GeometryFactory.createLine(p,m);
+
+		System.out.println();
+		System.out.println("Line Tests:");
 		Line l,s;
 		try {
 			// examples for line creation
 			l = GeometryFactory.createLine("LINESTRING Z(12 31 2,2 3 3,2 1 1,12 5 1)");
-			s = GeometryFactory.createLine(new double[][] {{12,31},{2,3.432,3},{2,111,1},{12,5,1}});
+			s = GeometryFactory.createLine(new double[][] {{12,31,2},{2,3.432,3},{2,111,1},{11, 43,1}});
 			System.out.println(s.getWktType());
 			try {
 				System.out.println(l.getWKT());
@@ -107,5 +99,61 @@ public class Main {
 		} catch (WktInvalidException | DimensionalException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println();
+		System.out.println("Polygon Tests:");
+		Point x1 = GeometryFactory.createPoint(3,4,6);
+		Point x2 = GeometryFactory.createPoint(4,5,9);
+		Point x3 = GeometryFactory.createPoint(6,2,10);
+
+		Point[] polyPoints = {x1,x2,x3,x1};
+		Point p1 = x1;
+		p1.setLrsValue(3.);
+		x2.setLrsValue(4.);
+		x3.setLrsValue(1.);
+		Polygon poly = null;
+		Polygon poly2 = null;
+		Polygon poly3 = null;
+
+		try {
+			poly = GeometryFactory.createPolygon(polyPoints);
+			poly2 = GeometryFactory.createPolygon("POLYGON ((35 10,45 45,15 40,10 20,35 10))");
+			poly3 = GeometryFactory.createPolygon(new double[][] {{12,31,2},{2,3.432,3},{2,111,1},{11, 43,1},{12,31,2}});
+		} catch (DimensionalException | InvalidPolygonException | WktInvalidException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			if (poly != null) {
+				System.out.println(poly.getWKT());
+			}
+			if (poly2 != null) {
+				System.out.println(poly2.getWKT());
+			}
+			if (poly3 != null) {
+				System.out.println(poly3.getWKT());
+			}
+
+		} catch (WKTRepresentationException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println("---Test FeaturePackage---");
+		System.out.println("PointFeature Tests:");
+		try {
+			PointFeature x = new PointFeature("Heidelberg", "POINT (8.685297 49.416404)");
+			System.out.println(x.getInfo());
+			System.out.println(x.getWKT());
+			System.out.println(x.getName());
+			System.out.println(x.getGeometry().getDimension());
+			System.out.println(x.getGeometry().getX());
+			System.out.println(x.getGeometry().getY());
+
+		} catch (WktInvalidException | WKTRepresentationException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
